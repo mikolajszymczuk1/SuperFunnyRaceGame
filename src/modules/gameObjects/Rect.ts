bb54075d6827760316237b07f37efe8d3369a432
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-03-11 02:35:45 +0100

    Created first main scene and example game object

---
+++ src/modules/gameObjects/Rect.ts
@@ -0,0 +1,41 @@
+import Phaser from 'phaser';
+
+export default class Rect extends Phaser.GameObjects.Rectangle {
+  private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  private speed = 200;
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    fillColor: number,
+  ) {
+    super(scene, x, y, width, height, fillColor);
+
+    scene.add.existing(this);
+    this.cursors = scene.input.keyboard.createCursorKeys();
+  }
+
+  preUpdate(time: number, delta: number): void {
+    const d = delta / 1000;
+
+    if (this.cursors.up.isDown) {
+      this.setY(this.y - (this.speed * d));
+    }
+
+    if (this.cursors.down.isDown) {
+      this.setY(this.y + (this.speed * d));
+    }
+
+    if (this.cursors.left.isDown) {
+      this.setX(this.x - (this.speed * d));
+    }
+
+    if (this.cursors.right.isDown) {
+      this.setX(this.x + (this.speed * d));
+    }
+  }
+}
---
+++ src/modules/scenes/Game/Game.ts
@@ -0,0 +1,14 @@
+import Phaser from 'phaser';
+import Rect from 'modules/gameObjects/Rect';
+
+export default class Game extends Phaser.Scene {
+  private rect: Rect;
+
+  constructor() {
+    super('game');
+  }
+
+  create(): void {
+    const rect = new Rect(this, 50, 50, 100, 100, 0xff0000);
+  }
+}
