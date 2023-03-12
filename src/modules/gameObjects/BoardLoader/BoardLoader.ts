29190f3eea3ab6a24efffaf251be33f427615fb2
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-03-12 16:55:49 +0100

    Finished testing board

--- src/modules/gameObjects/BoardLoader/BoardLoader.ts
+++ src/modules/gameObjects/BoardLoader/BoardLoader.ts
@@ -3,6 +3,10 @@ import Phaser from 'phaser';
 export default class BoardLoader {
   private map: Phaser.Tilemaps.Tilemap;
 
+  private layers: string[];
+
+  private tileset: Phaser.Tilemaps.Tileset;
+
   constructor(
     scene: Phaser.Scene,
     spritesheetKey: string,
@@ -11,14 +15,30 @@ export default class BoardLoader {
     layers: string[],
   ) {
     this.map = scene.make.tilemap({ key: mapKey });
-    const tileset = this.map.addTilesetImage(tilesetName, spritesheetKey);
+    this.layers = layers;
+    this.tileset = this.map.addTilesetImage(tilesetName, spritesheetKey);
+  }
 
-    let layer: Phaser.Tilemaps.TilemapLayer;
+  loadLayers(): Phaser.Tilemaps.TilemapLayer[] {
+    const tileLayers: Phaser.Tilemaps.TilemapLayer[] = [];
 
-    for (let i = 0; i < layers.length; i += 1) {
-      layer = this.map.createLayer(layers[i], tileset, 0, 0);
+    // Load base layers
+    for (let i = 0; i < this.layers.length; i += 1) {
+      const layer = this.map.createLayer(this.layers[i], this.tileset, 0, 0);
+      tileLayers.push(layer);
     }
 
-    layer.scale = 0.5;
+    return tileLayers;
+  }
+
+  loadSpawnPoint(): Phaser.Types.Tilemaps.TiledObject {
+    // Create spawn object
+    const [spawnPoints] = this.map.objects;
+    const [mainSpawnPoint] = spawnPoints.objects;
+    return mainSpawnPoint;
+  }
+
+  getMap(): Phaser.Tilemaps.Tilemap {
+    return this.map;
   }
 }
--- src/modules/gameObjects/Rect/Rect.ts
+++ src/modules/gameObjects/Rect/Rect.ts
@@ -1,42 +1,88 @@
 import Phaser from 'phaser';
 
-export default class Rect extends Phaser.GameObjects.Rectangle {
+export default class Rect extends Phaser.GameObjects.Container {
+  public scene: Phaser.Scene;
+
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
-  private speed = 200;
+  private rectSprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
+  readonly SPEED = 400;
+
+  readonly ROTATION_SPEED = 4;
 
   constructor(
     scene: Phaser.Scene,
     x: number,
     y: number,
-    width: number,
-    height: number,
-    fillColor: number,
   ) {
-    super(scene, x, y, width, height, fillColor);
-
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
+    super(scene, x, y);
+    this.scene = scene;
+    this.rectSprite = scene.physics.add.sprite(x, y, 'car');
     this.cursors = scene.input.keyboard.createCursorKeys();
+    this.scene.add.existing(this);
   }
 
-  preUpdate(time: number, delta: number): void {
-    const d = delta / 1000;
+  /** Return sprite object */
+  get sprite(): Phaser.Types.Physics.Arcade.SpriteWithDynamicBody {
+    return this.rectSprite;
+  }
 
-    if (this.cursors.up.isDown) {
-      this.setY(this.y - (this.speed * d));
-    }
+  preUpdate(): void {
+    this.detectInput();
+  }
 
-    if (this.cursors.down.isDown) {
-      this.setY(this.y + (this.speed * d));
-    }
+  /**
+   * Update rect position on user input
+   */
+  detectInput(): void {
+    this.rectSprite.setVelocity(0, 0);
 
     if (this.cursors.left.isDown) {
-      this.setX(this.x - (this.speed * d));
+      this.rectSprite.setAngle(this.rectSprite.angle - this.ROTATION_SPEED);
     }
 
     if (this.cursors.right.isDown) {
-      this.setX(this.x + (this.speed * d));
+      this.rectSprite.setAngle(this.rectSprite.angle + this.ROTATION_SPEED);
     }
+
+    if (this.cursors.up.isDown) {
+      const { vx, vy } = this.calculateNewVelocity();
+      this.rectSprite.setVelocity(vx, vy);
+    }
+
+    if (this.cursors.down.isDown) {
+      const { vx, vy } = this.calculateNewVelocity();
+      this.rectSprite.setVelocity(-vx, -vy);
+    }
+  }
+
+  /**
+   * Check if rect doesn't go out of board (X position)
+   * @returns true if rect is inside board (X position)
+   */
+  checkBoardEdgesX(newXVal: number): boolean {
+    const { width } = this.scene.sys.canvas;
+    return (newXVal - this.width / 2) > 0 && (newXVal + this.width / 2) < width;
+  }
+
+  /**
+   * Check if rect doesn't go out of board (Y position)
+   * @returns true if rect is inside board (Y position)
+   */
+  checkBoardEdgesY(newYVal: number): boolean {
+    const { height } = this.scene.sys.canvas;
+    return (newYVal - this.height / 2) > 0 && (newYVal + this.height / 2) < height;
+  }
+
+  /**
+   * Return velocity based on sprite angle
+   * @returns new velocity --> (vx, vy)
+   */
+  calculateNewVelocity(): { vx: number, vy: number } {
+    const vec = this.scene.physics.velocityFromAngle(this.rectSprite.angle, 1);
+    const newVx = vec.x * this.SPEED;
+    const newVy = vec.y * this.SPEED;
+    return { vx: newVx, vy: newVy };
   }
 }
