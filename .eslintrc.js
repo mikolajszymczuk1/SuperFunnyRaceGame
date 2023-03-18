9a3f79ddb119dca88174403c296d3959965ae0d2
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-03-18 22:53:03 +0100

    Some corrections

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended'
   ],
   rules: {
+    'import/no-named-as-default': 'off',
   },
 };
---
+++ src/modules/enums/EnvironmentEnum.ts
@@ -0,0 +1,8 @@
+enum EnvironmentEnum {
+  G = 9.81,
+  HP_TO_WATTS = 746,
+  AIR_DENSITY = 1.225,
+  ROAD_FRICTION = 0.9,
+}
+
+export default EnvironmentEnum;
